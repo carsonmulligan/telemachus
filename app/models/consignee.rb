d3461d490b5edd975bf2936ee0f7e4071441dfef
carson mulligan <carsonmulligan45@gmail.com>
2022-07-23 18:47:07 -0500

    Add model relationships

--- app/models/consignee.rb
+++ app/models/consignee.rb
@@ -1,3 +1,6 @@
 class Consignee < ApplicationRecord
   belongs_to :user
+  has_many :shipments
+  has_many :shippers
+  has_many :users
 end
--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -1,3 +1,6 @@
 class Shipper < ApplicationRecord
   belongs_to :user
+  has_many :shipments
+  has_many :consignees
+  has_many :users
 end
