16a27d6d089e7c160181e06124cdfc205b313a6a
carson mulligan <carsonmulligan45@gmail.com>
2022-07-23 18:55:25 -0500

    Add shipments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'shipments/new'
+  get 'shipments/create'
+  get 'shipments/index'
+  get 'shipments/edit'
+  get 'shipments/update'
+  get 'shipments/destroy'
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
