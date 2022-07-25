ac7fe252859884129451d5e0ac5d1cc802fc038e
carson mulligan <carsonmulligan45@gmail.com>
2022-07-24 21:36:01 -0500

    Add index and update params for Shipment controller

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,19 +1,22 @@
 class ShipmentsController < ApplicationController
+
+  def index
+    @shipments = Shipment.all
+  end
+
   def new
     @shipment = Shipment.new
   end
 
   def create
     @shipment = Shipment.new(shipment_params)
+    @shipment.user = current_user
 
     if @shipment.save
       redirect_to @shipper, notice: 'Shipment created successfully'
     else
       render :new
     end
-  end
-
-  def index
   end
 
   def edit
@@ -26,11 +29,13 @@ class ShipmentsController < ApplicationController
   end
 
   def show
+    @shipment = Shipment.find(params[:id])
   end
 
   private
 
   def shipment_params
-    params.require(:shipment).permit(:name, :user_id)
+    params.require(:shipment).permit(:origin, :destination, :transport_mode, :commodity, :reference_number, :shipper_id,
+                                     :consignee_id, :user_id)
   end
 end
